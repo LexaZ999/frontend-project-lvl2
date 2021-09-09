c5df536586e6ba788dac62064eddc4b431af85f0
Aleksey Zubkov <zubkovf@mail.ru>
2021-09-09 14:44:17 +0700

    Modify help.js

--- src/help.js
+++ src/help.js
@@ -6,6 +6,8 @@ const cli = () => {
   program
     .version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>')
     .parse(process.argv);
 };
 
