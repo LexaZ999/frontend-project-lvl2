ee035b61d07c6ef9c21961e4e3a34cf428307610
Aleksey Zubkov <zubkovf@mail.ru>
2021-09-30 14:44:43 +0700

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,10 @@
 import gendiff from '../index.js';
 import readFile from '../src/readFile.js';
 
-test('reverse', () => {
+test('gendiff', () => {
   expect(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json'))
     .toEqual(readFile('__fixtures__/expected_file.txt'));
-  expect('').toEqual('');
+
+  expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.yml'))
+    .toEqual(readFile('__fixtures__/expected_file.txt'));
 });
