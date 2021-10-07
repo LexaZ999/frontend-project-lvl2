7c4731a151390c594433bb42be75e2c8061a1118
Aleksey Zubkov <zubkovf@mail.ru>
2021-10-08 01:13:25 +0700

    Code refactoring astDiff.js

--- src/astDiff.js
+++ src/astDiff.js
@@ -1,67 +1,42 @@
 import _ from 'lodash';
 import getValue from './getValue.js';
 
-const getAstDiff = (obj1, obj2, depth = 0, status) => {
+const getAstDiff = (obj1, obj2, depth = 0, substatus) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const keys = keys1.concat(keys2);
   const uniqKeys = _.sortBy(keys.filter((key, index) => keys.indexOf(key) === index));
-
   const result = uniqKeys.flatMap((key) => {
     const val1 = getValue(obj1, key);
     const val2 = getValue(obj2, key);
+    let status;
+
+    const createDiffCell = (value1, value2, children) => {
+      const diffCell = {
+        key, value1, value2, depth, status, substatus, children,
+      };
+      return diffCell;
+    };
 
     if (typeof val1 === 'object' && typeof val2 === 'object') {
-      return {
-        name: key,
-        value1: 'nested',
-        value2: 'nested',
-        depth,
-        children: getAstDiff(val1, val2, depth + 1),
-      };
+      status = 'nested';
+      return createDiffCell('nested', 'nested', getAstDiff(val1, val2, depth + 1));
     }
-
     if (typeof val1 === 'object' && typeof val2 !== 'object') {
-      return {
-        name: key,
-        value1: 'nested',
-        value2: val2,
-        depth,
-        status,
-        children: getAstDiff(val1, {}, depth + 1, 'del'),
-      };
+      status = val2 ? 'nested changed to value' : 'deleted nested';
+      return createDiffCell('nested', val2, getAstDiff(val1, {}, depth + 1, 'del'));
     }
-
-    if (_.has(obj1, key) && !_.has(obj2, key) && typeof val1 === 'object') {
-      return {
-        name: key,
-        value1: 'nested',
-        value2: undefined,
-        depth,
-        children: getAstDiff(val1, {}, depth + 1),
-      };
+    if (!_.has(obj1, key) && _.has(obj2, key) && typeof val2 === 'object') {
+      status = 'added nested';
+      return createDiffCell(val1, 'nested', getAstDiff({}, val2, depth + 1, 'add'));
     }
-
-    if (_.has(obj2, key) && !_.has(obj1, key) && typeof val2 === 'object') {
-      return {
-        name: key,
-        value1: undefined,
-        value2: 'nested',
-        depth,
-        status,
-        children: getAstDiff({}, val2, depth + 1, 'add'),
-      };
+    if ((val1 === undefined) || (val2 === undefined)) {
+      status = (val1 === undefined) ? 'add' : 'del';
+      return createDiffCell(val1, val2);
     }
-
-    return {
-      name: key,
-      value1: val1,
-      value2: val2,
-      depth,
-      status,
-    };
+    status = (val1 === val2) ? 'unchanged' : 'changed';
+    return createDiffCell(val1, val2);
   });
   return result.flat();
 };
-
 export default getAstDiff;
