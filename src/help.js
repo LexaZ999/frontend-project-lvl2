18253f8fb81883bc39966feaf1bdcefeafb76136
Aleksey Zubkov <zubkovf@mail.ru>
2021-10-07 11:31:29 +0700

    Make stylish the default formatter

--- src/help.js
+++ src/help.js
@@ -7,10 +7,12 @@ const cli = () => {
   program
     .version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
+    .option('-f, --format [type]', 'output format', 'stylish')
     .arguments('<filepath1> <filepath2>')
     .action((filepath1, filepath2) => {
-      console.log(gendiff(filepath1, filepath2));
+      const format = `${program.opts().format}`;
+      console.log(gendiff(filepath1, filepath2, format));
+      console.log(format);
     })
     .parse(process.argv);
 };
