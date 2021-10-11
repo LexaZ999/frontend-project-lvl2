d3da57d85bc81ce37b78692a94dd09a867ada32c
Aleksey Zubkov <zubkovf@mail.ru>
2021-10-11 18:07:32 +0700

    Add tests for gendiff plain

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,18 @@
 import gendiff from '../index.js';
 import readFile from '../src/readFile.js';
 
-test('gendiff', () => {
+test('gendiff stylish', () => {
   expect(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json'))
-    .toEqual(readFile('__fixtures__/expected_file.txt'));
+    .toEqual(readFile('__fixtures__/stylish expected file.txt'));
 
   expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.yml'))
-    .toEqual(readFile('__fixtures__/expected_file.txt'));
+    .toEqual(readFile('__fixtures__/stylish expected file.txt'));
+});
+
+test('gendiff plain', () => {
+  expect(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain'))
+    .toEqual(readFile('__fixtures__/plain expected file.txt'));
+
+  expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain'))
+    .toEqual(readFile('__fixtures__/plain expected file.txt'));
 });
