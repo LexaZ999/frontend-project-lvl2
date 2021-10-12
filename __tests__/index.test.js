8a20fbe40b4efc3dd8d63c731d1aa33685a0c99d
Aleksey Zubkov <zubkovf@mail.ru>
2021-10-12 14:30:57 +0700

    Add tests for gendiff json

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,3 +16,11 @@ test('gendiff plain', () => {
   expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain'))
     .toEqual(readFile('__fixtures__/plain expected file.txt'));
 });
+
+test('gendiff json', () => {
+  expect(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json'))
+    .toEqual(readFile('__fixtures__/json expected file.json'));
+
+  expect(gendiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json'))
+    .toEqual(readFile('__fixtures__/json expected file.json'));
+});
