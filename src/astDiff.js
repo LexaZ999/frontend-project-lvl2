28ff5dd6abb46ae4d1a698b3bcf970594539f648
Aleksey Zubkov <zubkovf@mail.ru>
2021-10-18 10:39:54 +0700

    Add a null check to astDiff.js

--- src/astDiff.js
+++ src/astDiff.js
@@ -20,14 +20,14 @@ const getAstDiff = (obj1, obj2, path = '', depth = 0, substatus) => {
 
     const pathAndSeparator = (path === '') ? path : (path + point);
 
-    if (typeof val1 === 'object' && typeof val2 === 'object') {
+    if (typeof val1 === 'object' && val1 !== null && typeof val2 === 'object' && val2 !== null) {
       return createDiffCell('nested', 'nested', pathAndSeparator + key, 'nested', getAstDiff(val1, val2, pathAndSeparator + key, depth + 1));
     }
-    if (typeof val1 === 'object' && typeof val2 !== 'object') {
+    if (typeof val1 === 'object' && typeof val2 !== 'object' && val1 !== null) {
       const currentStatusNested = val2 ? 'nested changed to value' : 'deleted nested';
       return createDiffCell('nested', val2, pathAndSeparator + key, currentStatusNested, getAstDiff(val1, {}, pathAndSeparator + key, depth + 1, 'del'));
     }
-    if (!_.has(obj1, key) && _.has(obj2, key) && typeof val2 === 'object') {
+    if (!_.has(obj1, key) && _.has(obj2, key) && typeof val2 === 'object' && val2 !== null) {
       return createDiffCell(val1, 'nested', pathAndSeparator + key, 'added nested', getAstDiff({}, val2, pathAndSeparator + key, depth + 1, 'add'));
     }
     if ((val1 === undefined) || (val2 === undefined)) {
